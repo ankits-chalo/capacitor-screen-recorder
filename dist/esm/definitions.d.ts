c3393484446950f94d2b0238025d26fddb840559
Ankit Singhania <ankit.s@chalo.com>
2025-07-01 14:26:53 +0530

    Created a build and updated gitignore

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,13 @@
+export interface ScreenRecorderPlugin {
+    /**
+     * start the recording
+     * @param options Recording options
+     */
+    start(options?: {
+        recordAudio?: boolean;
+    }): Promise<void>;
+    /**
+     * stop the recording
+     */
+    stop(): Promise<void>;
+}
---
+++ dist/plugin.cjs.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var core = require('@capacitor/core');
+
+const ScreenRecorder = core.registerPlugin("ScreenRecorder", {
+    web: () => Promise.resolve().then(function () { return web; }).then((m) => new m.ScreenRecorderWeb()),
+});
+
+class ScreenRecorderWeb extends core.WebPlugin {
+    async start() {
+        throw new Error("Method not implemented.");
+    }
+    async stop() {
+        throw new Error("Method not implemented.");
+    }
+}
+
+var web = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    ScreenRecorderWeb: ScreenRecorderWeb
+});
+
+exports.ScreenRecorder = ScreenRecorder;
+//# sourceMappingURL=plugin.cjs.js.map
